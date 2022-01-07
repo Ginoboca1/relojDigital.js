bb39fc0d1aebeca812f005619fef887b9bafd780
Gino Boca <gnoboca@gmail.com>
2022-01-07 14:23:15 -0300

    optimizacion version mobile, logo 'gnoboc' añadido en ambas versiones

--- reloj.js
+++ reloj.js
@@ -1,26 +1,25 @@
-function mostrarReloj(){
+const mostrarReloj  = () => {
     let fecha = new Date();
-    let hr = formatoHora(fecha.getHours());
-    let min = formatoHora(fecha.getMinutes());
-    let sec = formatoHora(fecha.getSeconds());
-    document.getElementById('hora').innerHTML = `${hr}:${min}:${sec}`;
+    let hora = formatoHora(fecha.getHours());
+    let minutos = formatoHora(fecha.getMinutes());
+    let segundos = formatoHora(fecha.getSeconds());
+    document.getElementById('hora').innerHTML = `${hora}:${minutos}:${segundos}`;
 
+    const dias = ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'];
     const meses = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
-    const dias = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
 
     let diaSemana = dias[fecha.getDay()];
     let dia = fecha.getDate();
     let mes = meses[fecha.getMonth()];
-    
-    let fechaTexto = `${diaSemana}, ${dia}  ${mes}`;
-    document.getElementById('fecha').innerHTML = fechaTexto;
+    let fechaTexto = `${diaSemana}, ${dia} ${mes}`
+    document.getElementById('fecha').innerHTML = fechaTexto
 }
 
-setInterval(mostrarReloj, 1000);
+setInterval(mostrarReloj,1000);
 
-formatoHora = (hora) =>{
+const formatoHora = (hora) =>{
     if (hora < 10)
-    hora = '0' + hora;
-    return hora
+        hora = '0' + hora
+        return hora;
+    
 }
-
